5ce9c73c18edd045b5f3fad96d7a9cf5bb48b214
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-12-12 17:53:50 +0100

    Add build grid to pipeline

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,8 @@ defmodule Identicon do
     def main(string) do
         string
         |> hash
+        |> pick_color
+        |> build_grid
     end
 
     def hash(string) do
@@ -10,6 +12,20 @@ defmodule Identicon do
         |> :binary.bin_to_list
 
         %Identicon.Image{hex: hex}
+    end
+
+    def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+        %Identicon.Image{image | color: {red, green, blue}}
+    end
+
+    def build_grid(%Identicon.Image{hex: hex} = image) do
+        hex
+        |> Enum.chunk(3)
+        |> Enum.map(&mirror_row/1)
+    end
+
+    def mirror_row([first, second | _tail] = row) do
+        row ++ [first, second]
     end
 
 end
