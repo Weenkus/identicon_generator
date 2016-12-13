123e470df277cd54ef6df5ba7fc395aaf18872c1
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-12-13 17:38:09 +0100

    .gitignore update

--- lib/image.ex
+++
@@ -1,7 +0,0 @@
-defmodule Identicon.Image do
-    @moduledoc """
-      Struct used in Identicon generation.
-    """
-
-    defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
-end
--- test/identicon_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule IdenticonTest do
-  use ExUnit.Case
-  doctest Identicon
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
