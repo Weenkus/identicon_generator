dcadeb7284451e592671838e51f29fc42de33ef1
Vinko Kodzoman <vinko.kodzoman@fer.hr>
2016-12-13 16:47:08 +0100

    Finish project and add some examples

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,10 @@ defmodule Identicon do
         |> hash
         |> pick_color
         |> build_grid
+        |> filter_odd_squares
+        |> build_pixel_map
+        |> draw_image
+        |> save_image(string)
     end
 
     def hash(string) do
@@ -19,13 +23,58 @@ defmodule Identicon do
     end
 
     def build_grid(%Identicon.Image{hex: hex} = image) do
-        hex
-        |> Enum.chunk(3)
-        |> Enum.map(&mirror_row/1)
+        grid = 
+            hex
+            |> Enum.chunk(3)
+            |> Enum.map(&mirror_row/1)
+            |> List.flatten
+            |> Enum.with_index
+
+        %Identicon.Image{image | grid: grid}
     end
 
     def mirror_row([first, second | _tail] = row) do
-        row ++ [first, second]
+        row ++ [second, first]
+    end
+
+    def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+        grid = Enum.filter grid, fn({code, _index}) -> 
+            rem(code, 2) == 0
+        end
+
+        %Identicon.Image{image | grid: grid}
+    end
+
+    def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+        pixel_map = Enum.map grid, fn({_code, index}) ->
+            cell_width = 50
+            grid_width = 5
+
+            horizontal = rem(index, grid_width) * cell_width
+            vertical = div(index, grid_width) * cell_width
+
+            top_left = {horizontal, vertical}
+            bottom_left = {horizontal + cell_width, vertical + cell_width}
+
+            {top_left, bottom_left}
+        end
+
+        %Identicon.Image{image | pixel_map: pixel_map}
+    end
+
+    def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+        image = :egd.create(250, 250)
+        fill = :egd.color(color)
+
+        Enum.each pixel_map, fn({top_left, bottom_right}) ->
+            :egd.filledRectangle(image, top_left, bottom_right, fill)
+        end
+
+        :egd.render(image)
+    end
+
+    def save_image(image, filename) do
+        File.write("#{filename}.png", image)
     end
 
 end
